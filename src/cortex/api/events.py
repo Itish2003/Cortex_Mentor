b516cb2ee233c98952d3693851f22f2c90cb6882
Itish <itish.github.io@gmail.com>
2025-10-18 15:02:10 +0530

    fixing dependency injection and fixing redis issues

--- src/cortex/api/events.py
+++ src/cortex/api/events.py
@@ -1,16 +1,15 @@
-from fastapi import APIRouter, BackgroundTasks, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Request
 from cortex.models.events import GitCommitEvent
-from cortex.core.redis import get_redis
 from arq import ArqRedis
 
 router = APIRouter()
 
 @router.post("/events", status_code=status.HTTP_202_ACCEPTED)
-async def create_event(event: GitCommitEvent):
+async def create_event(event: GitCommitEvent, request: Request):
     """
     Accepts an event and enqueues it for processing.
     """
-    redis: ArqRedis = get_redis()
+    redis: ArqRedis = request.app.state.redis
     if not redis:
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
